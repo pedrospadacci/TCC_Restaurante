1bad001c162fe9f7b292be8bdbed184b237ef6e8
pspadacci <39204242+pedrospadacci@users.noreply.github.com>
2021-12-15 20:05:23 -0300

    Heroku

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-var express = requiser('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+const express = require('express')
+const path    = require('path')
+const cors = require("cors")
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
 
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started' + port)
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname, 'dist'))
+
+
+
+app.use(staticFileMiddleware)
+app.use(cors)
+
+
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/src/main.js'))
+})
+
+
+var port = process.env.PORT || 9090
+app.listen(port, function () {
+    console.log( 'Express serving on 9090!' )
+})
