50224f8deb6778b9e98212ca986f8f0373544bfe
Eduardo de Souza Santos Thais <dudu2541999@gmail.com>
2021-12-15 20:54:27 -0300

    Update Produtos.vue

--- src/components/Produtos.vue
+++ src/components/Produtos.vue
@@ -85,7 +85,7 @@ export default {
                 
                 }).then((saveResult)=> {
                     if (saveResult.isConfirmed){
-                        
+                        // vazio
                     }
                 })
             
