c343b072202100553c90031cef68c5bfa448b130
Eduardo de Souza Santos Thais <dudu2541999@gmail.com>
2021-12-15 21:50:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const staticFileMiddleware = express.static(path.join(__dirname, 'dist'))
 
 
 app.use(staticFileMiddleware)
-app.use(cors)
+app.use(cors())
 
 
 app.get('/', function (req, res) {
@@ -22,4 +22,12 @@ app.get('/', function (req, res) {
 var port = process.env.PORT || 9090
 app.listen(port, function () {
     console.log( 'Express serving on 9090!' )
+})
+
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
 })
